0db56418de2ba0dff1b86302e137702bec2ea456
mileusna <mileusna@gmail.com>
2017-06-26 04:36:42 +0200

    Add rich media messages

--- message.go
+++ message.go
@@ -19,9 +19,6 @@ import (
 }
 */
 
-// MessageType for viber messaging
-type MessageType string
-
 type messageResponse struct {
 	Status        int    `json:"status"`
 	StatusMessage string `json:"status_message"`
@@ -73,36 +70,40 @@ type VideoMessage struct {
 
 // Button for carousel
 type Button struct {
-	Columns    int    `json:"Columns"`
-	Rows       int    `json:"Rows"`
-	ActionType string `json:"ActionType"`
-	ActionBody string `json:"ActionBody"`
-	Image      string `json:"Image,omitempty"`
-	Text       string `json:"Text,omitempty"`
-	TextSize   string `json:"TextSize,omitempty"`
-	TextVAlign string `json:"TextVAlign,omitempty"`
-	TextHAlign string `json:"TextHAlign,omitempty"`
+	Columns    int        `json:"Columns"`
+	Rows       int        `json:"Rows"`
+	ActionType ActionType `json:"ActionType"`
+	ActionBody string     `json:"ActionBody"`
+	Image      string     `json:"Image,omitempty"`
+	Text       string     `json:"Text,omitempty"`
+	TextSize   TextSize   `json:"TextSize,omitempty"`
+	TextVAlign TextVAlign `json:"TextVAlign,omitempty"`
+	TextHAlign TextHAlign `json:"TextHAlign,omitempty"`
 }
 
 // RichMedia for carousel
 type RichMedia struct {
-	Type                string   `json:"Type"`
-	ButtonsGroupColumns int      `json:"ButtonsGroupColumns"`
-	ButtonsGroupRows    int      `json:"ButtonsGroupRows"`
-	BgColor             string   `json:"BgColor"`
-	TrackingData        string   `json:"tracking_data,omitempty"`
-	Buttons             []Button `json:"Buttons"`
+	Type                MessageType `json:"Type"`
+	ButtonsGroupColumns int         `json:"ButtonsGroupColumns"`
+	ButtonsGroupRows    int         `json:"ButtonsGroupRows"`
+	BgColor             string      `json:"BgColor"`
+	TrackingData        string      `json:"tracking_data,omitempty"`
+	Buttons             []Button    `json:"Buttons"`
 }
 
 // RichMediaMessage / Carousel
 type RichMediaMessage struct {
 	AuthToken     string      `json:"auth_token"`
-	Receiver      string      `json:"receiver"`
+	Receiver      string      `json:"receiver,omitempty"`
 	Type          MessageType `json:"type"`
 	MinAPIVersion int         `json:"min_api_version"`
 	RichMedia     RichMedia   `json:"rich_media"`
 }
 
+// MessageType for viber messaging
+type MessageType string
+
+// Message types
 const (
 	TypeTextMessage      = MessageType("text")
 	TypeURLMessage       = MessageType("url")
@@ -147,14 +148,7 @@ func (v *Viber) NewTextMessage(msg string) *TextMessage {
 	}
 }
 
-// SendTextMessage to reciever, returns message token
-func (v *Viber) SendTextMessage(receiver string, msg string) (msgToken uint64, err error) {
-	m := v.NewTextMessage(msg)
-	m.Receiver = receiver
-	return v.sendMessage("https://chatapi.viber.com/pa/send_message", m)
-}
-
-// NewURLMessage for viber
+// NewURLMessage creates new message with global sender and common params set
 func (v *Viber) NewURLMessage(msg string, url string) *URLMessage {
 	return &URLMessage{
 		TextMessage: TextMessage{
@@ -164,12 +158,6 @@ func (v *Viber) NewURLMessage(msg string, url string) *URLMessage {
 		},
 		Media: url,
 	}
-}
-
-// SendURLMessage to receiver, return message token
-func (v *Viber) SendURLMessage(receiver string, s Sender, msg string, url string) (msgToken uint64, err error) {
-	m := v.NewURLMessage(msg, url)
-	return v.sendMessage("https://chatapi.viber.com/pa/send_message", m)
 }
 
 // NewPictureMessage for viber
@@ -185,23 +173,76 @@ func (v *Viber) NewPictureMessage(msg string, url string, thumbURL string) *Pict
 	}
 }
 
+// SendTextMessage to reciever, returns message token
+func (v *Viber) SendTextMessage(receiver string, msg string) (msgToken uint64, err error) {
+	return v.SendMessage(receiver, v.NewTextMessage(msg))
+}
+
+// SendURLMessage to easily send url messages as global sender
+func (v *Viber) SendURLMessage(receiver string, msg string, url string) (msgToken uint64, err error) {
+	return v.SendMessage(receiver, v.NewURLMessage(msg, url))
+}
+
 // SendPictureMessage to receiver, returns message token
-// func (v *Viber) SendPictureMessage(receiver string, s Sender, msg string, url string, thumbURL string) (token int64, err error) {
-// 	m := PictureMessage{
-// 		Message: Message{
-// 			Receiver: receiver,
-// 			Sender:   s,
-// 			Type:     TypePictureMessage,
-// 			Text:     msg,
-// 		},
-// 		Media:     url,
-// 		Thumbnail: thumbURL,
-// 	}
+func (v *Viber) SendPictureMessage(receiver string, msg string, url string, thumbURL string) (token uint64, err error) {
+	return v.SendMessage(receiver, v.NewPictureMessage(msg, url, thumbURL))
+}
 
-// 	return v.sendMessage("https://chatapi.viber.com/pa/send_message", m)
-// }
+// NewRichMediaMessage creates new empty carousel message
+func (v *Viber) NewRichMediaMessage(cols, rows int, bgColor string) *RichMediaMessage {
+	return &RichMediaMessage{
+		MinAPIVersion: 2,
+		AuthToken:     v.AppKey,
+		Type:          TypeRichMediaMessage,
+		RichMedia: RichMedia{
+			Type:                TypeRichMediaMessage,
+			ButtonsGroupColumns: cols,
+			ButtonsGroupRows:    rows,
+			BgColor:             bgColor,
+		},
+	}
+}
 
-func (v *Viber) SendCarousel(receiver string) {
+// NewButton helper function for creating button with text and image
+func (v *Viber) NewButton(cols, rows int, typ ActionType, actionBody string, text, image string) Button {
+	return Button{
+		Columns:    cols,
+		Rows:       rows,
+		ActionType: typ,
+		ActionBody: actionBody,
+		Text:       text,
+		Image:      image,
+	}
+}
+
+// NewImageButton helper function for creating image button struct with common params
+func (v *Viber) NewImageButton(cols, rows int, typ ActionType, actionBody string, image string) Button {
+	return Button{
+		Columns:    cols,
+		Rows:       rows,
+		ActionType: typ,
+		ActionBody: actionBody,
+		Image:      image,
+	}
+}
+
+// NewTextButton helper function for creating image button struct with common params
+func (v *Viber) NewTextButton(cols, rows int, t ActionType, actionBody, text string) Button {
+	return Button{
+		Columns:    cols,
+		Rows:       rows,
+		ActionType: t,
+		ActionBody: actionBody,
+		Text:       text,
+	}
+}
+
+// AddButton to rich media message
+func (rm *RichMediaMessage) AddButton(b Button) {
+	rm.RichMedia.Buttons = append(rm.RichMedia.Buttons, b)
+}
+
+func (v *Viber) sendCarousel(receiver string) {
 	r := RichMediaMessage{
 		MinAPIVersion: 2,
 		Receiver:      receiver,
@@ -218,7 +259,7 @@ func (v *Viber) SendCarousel(receiver string) {
 	b1 := Button{
 		Columns:    6,
 		Rows:       3,
-		ActionType: "open-url",
+		ActionType: OpenURL,
 		ActionBody: "https://aviokarte.rs/",
 		Image:      "http://nstatic.net/beta/2b5b3ff1972f61d9bcfaaddd061aa1b9.jpg",
 	}
@@ -227,7 +268,7 @@ func (v *Viber) SendCarousel(receiver string) {
 	b1 = Button{
 		Columns:    6,
 		Rows:       2,
-		ActionType: "open-url",
+		ActionType: OpenURL,
 		ActionBody: "https://aviokarte.rs/",
 		Text:       "Košarkaši Crvene zvezde odbranili titulu šampiona Srbije",
 	}
@@ -236,12 +277,12 @@ func (v *Viber) SendCarousel(receiver string) {
 	b1 = Button{
 		Columns:    3,
 		Rows:       1,
-		ActionType: "reply",
+		ActionType: Reply,
 		ActionBody: "ID: 21432323",
 		Text:       "<font color=#ffffff>Otvori</font>",
-		TextSize:   "large",
-		TextVAlign: "middle",
-		TextHAlign: "middle",
+		TextSize:   Large,
+		TextVAlign: Middle,
+		TextHAlign: Center,
 		Image:      "https://s14.postimg.org/4mmt4rw1t/Button.png",
 	}
 	r.RichMedia.Buttons = append(r.RichMedia.Buttons, b1)
@@ -267,6 +308,7 @@ func (v *Viber) SendCarousel(receiver string) {
 
 // SendPublicMessage from public account
 func (v *Viber) SendPublicMessage(from string, m Message) (msgToken uint64, err error) {
+	// text, picture, video, file, location, contact, sticker and url
 	m.SetFrom(from)
 	return v.sendMessage("https://chatapi.viber.com/pa/post", m)
 }
@@ -286,3 +328,61 @@ func (m *TextMessage) SetReceiver(r string) {
 func (m *TextMessage) SetFrom(from string) {
 	m.From = from
 }
+
+// SetReceiver for text message
+func (rm *RichMediaMessage) SetReceiver(r string) {
+	rm.Receiver = r
+}
+
+// SetFrom to satisfy interface although RichMedia messages can't be sent to publich chat and don't have From
+func (rm *RichMediaMessage) SetFrom(from string) {}
+
+// TextSize for carousel buttons
+// viber.Small
+// viber.Medium
+// viber.Large
+type TextSize string
+
+// TextSize values
+const (
+	Small  = TextSize("small")
+	Medium = TextSize("medium")
+	Large  = TextSize("large")
+)
+
+// ActionType for carousel buttons
+// viber.Reply
+// viber.OpenURL
+type ActionType string
+
+// ActionType values
+const (
+	Reply   = ActionType("reply")
+	OpenURL = ActionType("open-url")
+)
+
+// TextVAlign for carousel buttons
+// viber.Top
+// viber.Middle
+// viber.Bottom
+type TextVAlign string
+
+// TextVAlign values
+const (
+	Top    = TextVAlign("top")
+	Middle = TextVAlign("middle")
+	Bottom = TextVAlign("bottom")
+)
+
+// TextHAlign for carousel buttons
+// viber.Left
+// viber.Center
+// viber.Middle
+type TextHAlign string
+
+// TextHAlign values
+const (
+	Left   = TextHAlign("left")
+	Center = TextHAlign("middle")
+	Right  = TextHAlign("right")
+)
