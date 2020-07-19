ae64c7ed9b85039c2b3892db736e7d047356c524
Pavel Sukhodoev <xcyxoux@gmail.com>
2020-07-19 22:37:55 +0300

    add input field state

--- keyboard.go
+++ keyboard.go
@@ -1,16 +1,31 @@
 package viber
 
+type InputFieldStateType string
+
+// InputFieldState Values
+const (
+	RegularState   = InputFieldStateType("regular")
+	MinimizedState = InputFieldStateType("minimized")
+	HiddenState    = InputFieldStateType("hidden")
+)
+
 // Keyboard struct
 type Keyboard struct {
-	Type          string   `json:"Type"`
-	DefaultHeight bool     `json:"DefaultHeight,omitempty"`
-	BgColor       string   `json:"BgColor,omitempty"`
-	Buttons       []Button `json:"Buttons"`
+	Type            string              `json:"Type"`
+	DefaultHeight   bool                `json:"DefaultHeight,omitempty"`
+	BgColor         string              `json:"BgColor,omitempty"`
+	Buttons         []Button            `json:"Buttons"`
+	InputFieldState InputFieldStateType `json:"InputFieldState,omitempty"`
 }
 
 // AddButton to keyboard
 func (k *Keyboard) AddButton(b *Button) {
 	k.Buttons = append(k.Buttons, *b)
+}
+
+func (k *Keyboard) SetInputFieldState(state InputFieldStateType) *Keyboard {
+	k.InputFieldState = state
+	return k
 }
 
 // NewKeyboard struct with attribs init
